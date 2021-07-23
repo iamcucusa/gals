079de1c38d0f7505c5355727afa1c5bd0742a17e
Grace Henriquez <graciela.fernandez@businesspartner.roche.com>
2021-07-23 18:34:19 +0200

    Initial Commit

---
+++ gals-1/gals_1App.swift
@@ -0,0 +1,17 @@
+//
+//  gals_1App.swift
+//  gals-1
+//
+//  Created by Graciela Fernandez on 23/7/21.
+//
+
+import SwiftUI
+
+@main
+struct gals_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
